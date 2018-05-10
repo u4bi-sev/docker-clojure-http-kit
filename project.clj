4c06b349202404e7d7f6f5ad82b8299ffde2bd10
u4bi <admin@naver.id>
2018-05-10 15:02:52 +0900

    Added profiles to project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject c-http-kit "0.1.0-SNAPSHOT"
+(defproject c-http-kit "0.1.0"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.6.1"]
                  [http-kit "2.2.0"]]
   :jvm-opts ["--add-modules" "java.xml.bind"]
-  :main c-http-kit.core)
+  :main c-http-kit.core
+  :profiles {:uberjar {:aot :all}})
