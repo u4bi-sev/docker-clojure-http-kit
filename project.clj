e4e3a9b524ef46f7d98b6e84dc99c1f1d8948628
u4bi <admin@naver.id>
2018-05-10 22:28:58 +0900

    Added ring.middleware and servlet-api handler.

--- project.clj
+++ project.clj
@@ -2,7 +2,10 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.6.1"]
-                 [http-kit "2.2.0"]]
+                 [http-kit "2.2.0"]
+                 [ring/ring-json "0.4.0"]
+                 [ring-cors "0.1.12"]
+                 [javax.servlet/servlet-api "2.5"]]
   :jvm-opts ["--add-modules" "java.xml.bind"]
   :main c-http-kit.core
   :profiles {:uberjar {:aot :all}})
