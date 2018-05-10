6f97ba328c39fbeff83412757ed804af5d426f8f
u4bi <admin@naver.id>
2018-05-10 13:02:29 +0900

    Added http kit server and compojure handler.

--- server/src/c_http_kit/core.clj
+++ server/src/c_http_kit/core.clj
@@ -1,6 +1,13 @@
-(ns c-http-kit.core)
+(ns c-http-kit.core
+  (:use org.httpkit.server)
+  (:require [compojure.core :refer [defroutes GET POST PUT DELETE context]]
+            [compojure.route :refer [resources not-found]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defroutes handler
+           (GET "/" [] "<h1>u4bi</h1>")
+           (resources "/")
+           (not-found "<h1>page not found</h1>"))
+
+(defn -main []
+  (run-server #'handler {:port 8080})
+  (println "Started server on port 8080"))
