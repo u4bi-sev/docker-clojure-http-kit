489f94dceb6728e5f86936987f0996d585bc09d4
u4bi <admin@naver.id>
2018-05-10 15:04:48 +0900

    Added env to PORT.

--- src/c_http_kit/core.clj
+++ src/c_http_kit/core.clj
@@ -1,7 +1,8 @@
 (ns c-http-kit.core
   (:use org.httpkit.server)
   (:require [compojure.core :refer [defroutes GET POST PUT DELETE context]]
-            [compojure.route :refer [resources not-found]]))
+            [compojure.route :refer [resources not-found]])
+  (:gen-class))
 
 (defroutes handler
            (GET "/" [] "<h1>u4bi</h1>")
@@ -9,5 +10,6 @@
            (not-found "<h1>page not found</h1>"))
 
 (defn -main []
-  (run-server #'handler {:port 8080})
-  (println "Started server on port 8080"))
+  (let [port (read-string (or (System/getenv "PORT") "3000"))]
+  (run-server #'handler {:port port})
+  (println "Started server on port" port)))
